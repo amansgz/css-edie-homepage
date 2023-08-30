0ac1795b48adc131ecf297f0db46b271b291be19
amansgz <amansgz.ar@gmail.com>
2023-08-30 20:57:16 -0300

    Add hide menu nav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const iconHamburger = document.getElementById('icon-hamburger');
 const iconClose = document.getElementById('icon-close');
 const navMenu = document.getElementById('nav-menu');
+const nav_Menu = document.querySelector('.nav__menu');
 
 iconHamburger.addEventListener('click', e => {
   iconHamburger.classList.toggle('hidden');
@@ -13,3 +14,12 @@ iconClose.addEventListener('click', () => {
   iconClose.classList.toggle('show');
   navMenu.classList.remove('nav__menu__show');
 })
+
+
+document.addEventListener("click", (e) => {
+    if (!e.target.matches(".nav__menu a")) return false;
+
+    iconHamburger.classList.toggle('hidden');
+    iconClose.classList.toggle('show');
+    nav_Menu.classList.remove('nav__menu__show');
+});
