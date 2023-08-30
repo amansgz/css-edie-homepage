7c23896eb004446d403508add4b0a9d4d68bc9ca
amansgz <amansgz.ar@gmail.com>
2023-08-30 20:14:27 -0300

    Update styles. Change social icons. Add menu nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const iconHamburger = document.getElementById('icon-hamburger');
+const iconClose = document.getElementById('icon-close');
+const navMenu = document.getElementById('nav-menu');
+
+iconHamburger.addEventListener('click', e => {
+  iconHamburger.classList.toggle('hidden');
+  iconClose.classList.add('show');
+  navMenu.classList.add('nav__menu__show');
+});
+
+iconClose.addEventListener('click', () => {
+  iconHamburger.classList.toggle('hidden');
+  iconClose.classList.toggle('show');
+  navMenu.classList.remove('nav__menu__show');
+})
